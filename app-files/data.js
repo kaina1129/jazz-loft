4c39aa68c6b4bc985d865b54c5a9623779413d5b
WillHHH <willsihangw@gmail.com>
2021-04-20 22:55:16 -0400

    update

---
+++ app-files/data.js
@@ -0,0 +1,489 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1_lobby",
+      "name": "1_Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.0002620240977027,
+        "pitch": 0.23022840337325334,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8734904839745212,
+          "pitch": 0.14539832429986355,
+          "rotation": 18.84955592153877,
+          "target": "3-1a_-bernard_seeman_gallery"
+        },
+        {
+          "yaw": 0.9678029744972303,
+          "pitch": 0.30203319800980566,
+          "rotation": 0,
+          "target": "2-2_hall_gallery"
+        },
+        {
+          "yaw": 2.584453081329894,
+          "pitch": 0.1635306767628144,
+          "rotation": 0.7853981633974483,
+          "target": "1-6_main_stage"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.50762126950233,
+          "pitch": 0.007351541285309793,
+          "title": "Fancy Fur",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.138942179688094,
+          "pitch": 0.03689197864130733,
+          "title": "Concert Gown",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.1987520338817355,
+          "pitch": 0.10289304149815592,
+          "title": "Trotman Bass",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.9798910077435927,
+          "pitch": 0.23769960514837507,
+          "title": "Grammy Award",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-6_main_stage",
+      "name": "6_Main_Stage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8441187104544117,
+          "pitch": 0.10856069356436038,
+          "rotation": 0.7853981633974483,
+          "target": "4-6a_library"
+        },
+        {
+          "yaw": 1.6914453086715575,
+          "pitch": 0.23471246223761533,
+          "rotation": 0,
+          "target": "6-5_teddy_charles_room"
+        },
+        {
+          "yaw": 2.5942376960859974,
+          "pitch": -0.025512500094148294,
+          "rotation": 0.7853981633974483,
+          "target": "0-1_lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2_hall_gallery",
+      "name": "2_Hall_Gallery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5044983620459949,
+          "pitch": 0.39875697332027826,
+          "rotation": 1.5707963267948966,
+          "target": "9-3_milt_hinton_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1a_-bernard_seeman_gallery",
+      "name": "1A_ Bernard_Seeman_Gallery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7067601919382778,
+          "pitch": 0.35692085055764267,
+          "rotation": 0,
+          "target": "0-1_lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-6a_library",
+      "name": "6A_Library",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.536093558460351,
+          "pitch": 0.15423800734728133,
+          "rotation": 5.497787143782138,
+          "target": "1-6_main_stage"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0888758553691495,
+          "pitch": 0.14828375247423153,
+          "title": "Bass Saxophone",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.0714391511033172,
+          "pitch": 0.6298456865633781,
+          "title": "Foxxy",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-5a",
+      "name": "5A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-5_teddy_charles_room",
+      "name": "5_Teddy_Charles_Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.852122034871063,
+          "pitch": 0.43373134588868467,
+          "rotation": 5.497787143782138,
+          "target": "5-5a"
+        },
+        {
+          "yaw": -2.587766144945192,
+          "pitch": 0.3477402790189199,
+          "rotation": 0.7853981633974483,
+          "target": "1-6_main_stage"
+        },
+        {
+          "yaw": -0.8028975420102675,
+          "pitch": 0.4393633299541424,
+          "rotation": 5.497787143782138,
+          "target": "9-3_milt_hinton_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-4a_wine_cellar",
+      "name": "4A_Wine_Cellar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15943383786344612,
+          "pitch": 0.43970362244454186,
+          "rotation": 0,
+          "target": "8-4_coal_bin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-4_coal_bin",
+      "name": "4_Coal_Bin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6147837922517638,
+          "pitch": 0.20767006169248958,
+          "rotation": 5.497787143782138,
+          "target": "7-4a_wine_cellar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-3_milt_hinton_room",
+      "name": "3_Milt_Hinton_Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2754344376409676,
+          "pitch": 0.1548337757539784,
+          "rotation": 0.7853981633974483,
+          "target": "6-5_teddy_charles_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-1st-fl-hallway",
+      "name": "1st Fl Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "JazzLoft",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
